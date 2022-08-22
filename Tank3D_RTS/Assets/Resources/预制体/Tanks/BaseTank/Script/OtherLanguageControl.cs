56f4de35338e273c91e93e29b393255ea472206d
y-yunheng <75204142+y-yunheng@users.noreply.github.com>
2022-08-22 20:52:48 +0800

    1.2

--- Tank3D_RTS/Assets/Resources/预制体/Tanks/BaseTank/Script/OtherLanguageControl.cs
+++ Tank3D_RTS/Assets/Resources/预制体/Tanks/BaseTank/Script/OtherLanguageControl.cs
@@ -17,7 +17,9 @@ public class OtherLanguageControl : MonoBehaviour
         string sendmsg=GetMsg();
         mp.WriteMemory(sendmsg,"Cshape");
         string readstring=mp.ReadMemory("Python");
-        AndroidJavaObject 
+        /*拿到string后转为JObject对象，即转为json
+          将数据写入AgentInfo
+         */
     }
     string GetMsg()
     {
